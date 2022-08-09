44711a065c9dfb8871fa25db6848120e7bdd0b49
array16 <88081287+array16@users.noreply.github.com>
2022-08-09 10:52:07 +0700

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import time
 my_bar = st.progress(0)
 for i in range(100):
     my_bar.progress(i + 1)
-    time.sleep(0.01)
+    time.sleep(0.04)
 
 my_bar.empty()  # Remove the progress bar
 st.success('hello welcome to arrayweb')
